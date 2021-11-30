ca5d247ebf29395798c8e48672251915e441b7a7
ChudsV <jvchudek90@gmail.com>
2021-11-30 16:34:48 -0300

    main

---
+++ README.md
@@ -0,0 +1,19 @@
+# EP1-SD
+
+Para o funcionamento correto entre cliente e servidor basta executar a classe EQ2_AssistenteVacinacao, será aberto as telas de cliente e servidor.
+
+Na tela do servidor é necessário inserir a porta em que o servidor será aberto e clicar no botão Iniciar Servidor.
+
+Na tela de cliente é necessário se conectar primeiro, primeiramente clicar no botão Conectar e inserir o host (ip) destino do servidor, juntamente com a porta.
+Após essa validação é possível realizar o login.
+
+Email e senha valida:
+Email : teste@teste.com
+Senha: 12345
+
+Após inserir as credenciais clicar no botão enviar.
+O programa irá verificar se as informações estão corretas, se sim aparecerá a tela Home Page.
+
+Na tela Home Page é possivel realizar o logout.
+
+Bibliotecas do json estão instaladas na pasta \Eco\src\org dentro do arquivo do projeto.
---
+++ src/Usuario/Usuario.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Usuario;
+
+import java.net.Socket;
+
+
+
+/**
+ *
+ * @author sopro
+ */
+public class Usuario{
+
+    /**
+     * @return the socket
+     */
+    public Socket getSocket() {
+        return socket;
+    }
+
+    /**
+     * @param socket the socket to set
+     */
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the id
+     */
+    
+    private String email;
+    private String nome;
+    private String senha;
+    private Socket socket;
+    private int id;
+    
+    
+    public Usuario(String email, String senha, String nome){
+        setEmail(email);
+        setSenha(senha);
+        setNome(nome);
+    }
+    
+    
+}
